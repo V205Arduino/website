62d3da1a99072a52a3bb2074a08899d67a788593
V205 <138826219+V205Arduino@users.noreply.github.com>
2024-09-22 14:07:40 -0700

    test

--- deploy.sh
+++ deploy.sh
@@ -1,8 +1,11 @@
-cd 
-cd website
-export COMMIT_HASH=$(git rev-parse HEAD) 
-date +"%r"
+#cd 
+#cd website
+ls
 
-export BUILD_TIME=$(date +"%r")
+export COMMIT_HASH=$(git rev-parse HEAD) 
+#date +"%r"
+
+#export BUILD_TIME=$(date +"%r")
+export BUILD_TIME=$(date)
 npm run build
 cp -r dist output_done
