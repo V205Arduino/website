4c3f232151cdf1aa2ca57d6d1d03ce019d5741ec
V205 <138826219+V205Arduino@users.noreply.github.com>
2024-10-04 10:37:39 -0700

    test

--- src/components/FetchData.svelte
+++ src/components/FetchData.svelte
@@ -4,8 +4,8 @@
     let error = null;
   
     // Specify your API endpoint here
-    const apiUrl = 'https://scrapbook.hackclub.com/api/users';
-  
+    const apiUrl ='https://scrapbook.hackclub.com/api/users'; //'https://localhost:3000/API'//
+    console.log("doing stuff")
     // Fetch data from the API
     async function fetchData() {
       try {
@@ -14,25 +14,30 @@
           throw new Error('Failed to fetch data');
         }
         data = await response.json();
+
       } catch (err) {
         error = err.message;
+        console.log("aaa err")
       } finally {
         loading = false;
+        console.log("test")
       }
     }
+
   
     // Fetch data when the component is mounted
     fetchData();
   </script>
   
   {#if loading}
-    <p>Loading...</p>
+    <p>Loading... <br>Please wait as this can sadly take up to 15 seconds on average.</p>
   {:else if error}
     <p>Error: {error}</p>
   {:else}
     <ul>
       {#each data as item}
         <li>{item.title}</li>
+
       {/each}
     </ul>
   {/if}
